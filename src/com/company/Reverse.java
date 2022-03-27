85c5e3dcebd12de7f5b4932b4a1f53c1205e385c
Nikolay Mishin <nikolay.mishin@macys.com>
2022-03-27 06:49:19 +0300

    add chack input parameters

--- src/com/company/Reverse.java
+++ src/com/company/Reverse.java
@@ -2,20 +2,23 @@ package com.company;
 
 public class Reverse {
     public static void main(String[] args) {
-        String forReverse= "My name is Nikolay!";
-        System.out.println("reverse("+forReverse+") = " + reverse(forReverse));
+        String forReverse = "My name is Nikolay!";
+        System.out.println("reverse(" + forReverse + ") = " + reverse(forReverse));
     }
 
-    static  String reverse(String str){
-    String[] arrayChar = str.split("");
+    static String reverse(String str) {
+        if (str == null || str.length() < 2) {
+            return "hmm that is not good";
+        }
+        String[] arrayChar = str.split("");
         char[] chars = str.toCharArray();
         String reversedString = "";
-        System.out.println("revercedString = "+reversedString);
-        System.out.println("arrayChar.length = "+arrayChar.length);
-        System.out.println("arrayChar[arrayChar.length-1] = "+arrayChar[arrayChar.length-1]);
+        System.out.println("revercedString = " + reversedString);
+        System.out.println("arrayChar.length = " + arrayChar.length);
+        System.out.println("arrayChar[arrayChar.length-1] = " + arrayChar[arrayChar.length - 1]);
         for (int i = chars.length - 1; i >= 0; i--) {
-            reversedString = reversedString+arrayChar[i];
-            System.out.println("reversedString = "+reversedString);
+            reversedString = reversedString + arrayChar[i];
+            System.out.println("reversedString = " + reversedString);
         }
         return reversedString;
     }
